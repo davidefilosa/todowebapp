eaf01ed21bdc15595f7ab7c165e9e7addb6a10ea
Davide Filosa <davideclaypool@gmail.com!>
2022-11-10 12:49:38 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,9 @@
+def get_todos():
+    with open('todos.txt', 'r') as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos):
+    with open('todos.txt', 'w') as file:
+        file.writelines(todos)
---
+++ web.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ''
+
+
+st.title('My To-do App')
+st.subheader('This are my to-do for today:')
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo.strip('\n'))
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo.strip('\n')]
+        st.experimental_rerun()
+
+
+st.text_input(label='Enter a todo',
+              placeholder='Enter a todo...',
+              label_visibility='hidden',
+              on_change=add_todo,
+              key='new_todo')
+
